2492f6c1516ee9687b9f0d1146ee4d6d745dc7f3
Mikkel Buhl <52383121+Zorxh@users.noreply.github.com>
2020-06-15 18:13:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToeDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
